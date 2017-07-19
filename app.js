e9ce3e097cd605d520e38793354a0ed7906ab08b
Sevdi Palazliev <palazliev@sevdi-pc>
2017-07-20 00:40:00 +0300

    * Init commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var auth = require('./routes/auth');
+
+var app = express();
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy(
+    {
+	clientID: '183929068166-nn4flm489t9k0g4niki0ikausmspthel.apps.googleusercontent.com',
+	clientSecret: 'srRQYgwuKowcaOCDxG582WYf',
+	callbackURL: 'http://localhost:3000/auth/google/callback/'
+    },
+    function(accessToken, refreshToken, profile, done) {
+	return done(null, profile);
+    }
+))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret: 'anything'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/auth', auth);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
